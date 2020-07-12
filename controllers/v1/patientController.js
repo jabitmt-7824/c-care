fbeaa04a9818c2ef483111a003fe37edf0bf2934
Jabir MT <jabirmt7824@gmail.com>
2020-07-12 18:51:20 +0530

    all reports of a patient completed

--- controllers/v1/patientController.js
+++ controllers/v1/patientController.js
@@ -29,11 +29,41 @@ module.exports.createReport = async function (req, res) {
     try {
         let patient = await Patient.findById(req.params.id);
         if (patient) {
-            let report = await Report.create({doctor:req.user._id, patient: req.params.id, status: req.body.status, date: req.body.date });
+            let report = await Report.create({ doctor: req.user._id, patient: req.params.id, status: req.body.status, date: req.body.date });
             patient.reports.push(report);
             patient.save();
             return res.status(200).json({
                 message: "Report Created"
+            });
+        }
+        else {
+            return res.status(409).json({
+                message: "This Patient not Registered In this System"
+            });
+        }
+    } catch (err) {
+        console.log("Error", err);
+        return res.status(500), json({
+            message: "Internal Server Error"
+        });
+    }
+}
+
+module.exports.allReports = async function (req, res) {
+    try {
+        let patient = await Patient.findById(req.params.id).
+            populate({
+                path: "reports",
+                populate: {
+                   path: "doctor",
+                   select: "name registration_no -_id"
+                }
+            });
+
+        if (patient) {
+            return res.status(200).json({
+                message: `Reports of ${patient.name} from oldest to latest`,
+                reports: patient.reports
             });
         }
         else {
--- routes/v1/patients.js
+++ routes/v1/patients.js
@@ -6,5 +6,6 @@ const patientController = require("../../controllers/v1/patientController");
 
 router.post("/register", passport.authenticate("jwt",{session:false}), patientController.patientRegister);
 router.post("/:id/create_report", passport.authenticate("jwt",{session:false}), patientController.createReport);
+router.get("/:id/all_reports", passport.authenticate("jwt",{session:false}), patientController.allReports);
 
 module.exports = router;
